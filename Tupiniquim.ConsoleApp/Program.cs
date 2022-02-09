fa36e0bd12a2a312e89a10a132ae9598e0c4f7b4
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-09 16:01:57 -0300

    create tupiniquim repository

--- Tupiniquim.ConsoleApp/Program.cs
+++ Tupiniquim.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace Tupiniquim.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
