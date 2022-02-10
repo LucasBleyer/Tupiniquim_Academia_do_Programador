39b0989b293099aff3f80a630f166903d06ff2cd
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-10 10:36:53 -0300

    entradas

--- Tupiniquim.ConsoleApp/Program.cs
+++ Tupiniquim.ConsoleApp/Program.cs
@@ -6,7 +6,16 @@ namespace Tupiniquim.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Tupiniquim I");
+            
+            Console.WriteLine("\r\nDigite as Coordenadas para Tupiniquim I mapear, por exemplo = 0 0");
+            Console.Write("Coordenadas: ");
+            string coordenadas = Console.ReadLine();
 
+            Console.WriteLine("\r\nDigite a Posição Inicial para Tupiniquim I começar, por exemplo = 0 0 N");
+            Console.WriteLine("N = Norte, S = Sul, L = Leste, O = Oeste");
+            Console.Write("Posição Inicial: ");
+            string posicao_inicial = Console.ReadLine();
 
          
         }
